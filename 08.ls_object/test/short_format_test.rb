f91f4009c28cc9cb8a1c568bed170cee08eccdfa
mami-inuzuka <inuzuka.mami@gmail.com>
2021-10-13 11:25:52 +0900

    テストケースの追加

--- 08.ls_object/test/short_format_test.rb
+++ 08.ls_object/test/short_format_test.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
 require 'minitest/autorun'
-require './lib/ls_command'
 require './lib/short_format'
-require 'pathname'
 
 class ShortFormatTest < Minitest::Test
   TARGET_PATHNAME = 'test/fixtures/sample-app'
@@ -15,7 +13,7 @@ class ShortFormatTest < Minitest::Test
       Procfile                app.json                config.ru               package.json            vendor
       README.md               babel.config.js         db                      postcss.config.js       yarn.lock
     TEXT
-    assert_equal expected, LS::ShortFormat.new(TARGET_PATHNAME, 123).list
+    assert_equal expected, LS::ShortFormat.new(pathname: TARGET_PATHNAME, width: 123).list
   end
 
   def test_run_ls_width_106
@@ -26,7 +24,7 @@ class ShortFormatTest < Minitest::Test
       README.md               bin                     log                     vendor
       Rakefile                config                  package.json            yarn.lock
     TEXT
-    assert_equal expected, LS::ShortFormat.new(TARGET_PATHNAME, 106).list
+    assert_equal expected, LS::ShortFormat.new(pathname: TARGET_PATHNAME, width: 106).list
   end
 
   def test_run_ls_width_72
@@ -39,7 +37,7 @@ class ShortFormatTest < Minitest::Test
       app                     lib                     yarn.lock
       app.json                log
     TEXT
-    assert_equal expected, LS::ShortFormat.new(TARGET_PATHNAME, 72).list
+    assert_equal expected, LS::ShortFormat.new(pathname: TARGET_PATHNAME, width: 72).list
   end
 
   def test_run_ls_width_37
@@ -65,7 +63,7 @@ class ShortFormatTest < Minitest::Test
       vendor
       yarn.lock
     TEXT
-    assert_equal expected, LS::ShortFormat.new(TARGET_PATHNAME, 37).list
+    assert_equal expected, LS::ShortFormat.new(pathname: TARGET_PATHNAME, width: 37).list
   end
 
   def test_run_ls_width_1
@@ -91,6 +89,29 @@ class ShortFormatTest < Minitest::Test
       vendor
       yarn.lock
     TEXT
-    assert_equal expected, LS::ShortFormat.new(TARGET_PATHNAME, 1).list
+    assert_equal expected, LS::ShortFormat.new(pathname: TARGET_PATHNAME, width: 1).list
   end
+
+  def test_run_ls_reverse
+    expected = <<~TEXT.chomp
+      yarn.lock               postcss.config.js       db                      babel.config.js         README.md
+      vendor                  package.json            config.ru               app.json                Procfile
+      test                    log                     config                  app                     Gemfile.lock
+      public                  lib                     bin                     Rakefile                Gemfile
+    TEXT
+    assert_equal expected, LS::ShortFormat.new(pathname: TARGET_PATHNAME, width: 123, reverse: true).list
+  end
+
+  def test_run_ls_dot_match
+    expected = <<~TEXT.chomp
+      .                       .node-version           README.md               config                  postcss.config.js
+      ..                      .rubocop.yml            Rakefile                config.ru               public
+      .browserslistrc         .ruby-version           app                     db                      test
+      .eslintrc               Gemfile                 app.json                lib                     vendor
+      .github                 Gemfile.lock            babel.config.js         log                     yarn.lock
+      .gitignore              Procfile                bin                     package.json
+    TEXT
+    assert_equal expected, LS::ShortFormat.new(pathname: TARGET_PATHNAME, width: 123, dot_match: true).list
+  end
+
 end
