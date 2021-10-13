658acd721a4ef3a5feb7efa9ee3f5ff74e304bd2
mami-inuzuka <inuzuka.mami@gmail.com>
2021-10-13 11:30:28 +0900

    外から呼び出さないメソッドをprivateに

--- 08.ls_object/lib/short_format.rb
+++ 08.ls_object/lib/short_format.rb
@@ -10,17 +10,10 @@ module LS
     end
 
     def list
-      max_file_path_count = @collected_files.max_file_path_count
-      file_paths = @collected_files.file_paths
-      col_count = @width / (max_file_path_count + 7)
-      row_count = col_count.zero? ? file_paths.count : (file_paths.count.to_f / col_count).ceil
-      transposed_file_paths = safe_transpose(file_paths.each_slice(row_count).to_a)
       format_table(transposed_file_paths, max_file_path_count)
     end
 
-    def safe_transpose(nested_file_name)
-      nested_file_name[0].zip(*nested_file_name[1..-1])
-    end
+    private
 
     def format_table(file_paths, max_file_path_count)
       file_paths.map do |row_files|
@@ -28,11 +21,26 @@ module LS
       end.join("\n")
     end
 
+    def max_file_path_count
+      @collected_files.max_file_path_count
+    end
+
     def render_short_format_row(row_files, max_file_path_count)
       row_files.map do |file_path|
         basename = file_path ? ::File.basename(file_path) : ''
         basename.ljust(max_file_path_count + 7)
       end.join.rstrip
+    end
+
+    def safe_transpose(nested_file_name)
+      nested_file_name[0].zip(*nested_file_name[1..-1])
+    end
+
+    def transposed_file_paths
+      file_paths = @collected_files.file_paths
+      col_count = @width / (max_file_path_count + 7)
+      row_count = col_count.zero? ? file_paths.count : (file_paths.count.to_f / col_count).ceil
+      safe_transpose(file_paths.each_slice(row_count).to_a)
     end
   end
 end
