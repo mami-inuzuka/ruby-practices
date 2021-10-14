05fc86947df8d84df41f12a8fe69f591d684c8c3
mami-inuzuka <inuzuka.mami@gmail.com>
2021-10-14 10:43:38 +0900

    一括でテストを行うためのrakeファイルを作成

---
+++ 08.ls_object/rakefile.rb
@@ -0,0 +1,11 @@
+# coding: UTF-8
+
+require 'rake/testtask'
+
+task :default => [:test]
+
+Rake::TestTask.new do |t|
+  t.libs << "test"
+  t.test_files = Dir['test/*_test.rb']
+  t.verbose = true
+end
