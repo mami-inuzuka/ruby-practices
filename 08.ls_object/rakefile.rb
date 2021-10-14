06d848c4365a1747bf8f1a412503e125643f1911
mami-inuzuka <inuzuka.mami@gmail.com>
2021-10-14 13:27:29 +0900

    rubocopエラーの解消

--- 08.ls_object/rakefile.rb
+++ 08.ls_object/rakefile.rb
@@ -1,11 +1,11 @@
-# coding: UTF-8
+# frozen_string_literal: true
 
 require 'rake/testtask'
 
-task :default => [:test]
+task default: [:test]
 
 Rake::TestTask.new do |t|
-  t.libs << "test"
+  t.libs << 'test'
   t.test_files = Dir['test/*_test.rb']
   t.verbose = true
 end
