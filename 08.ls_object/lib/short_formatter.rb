b876bf5c809891824e342edf47d1c52b9c801d7b
mami-inuzuka <inuzuka.mami@gmail.com>
2021-10-14 09:01:35 +0900

    メソッド名の変更

--- 08.ls_object/lib/short_formatter.rb
+++ 08.ls_object/lib/short_formatter.rb
@@ -12,37 +12,37 @@ module LS
     end
 
     def list
-      format_table(transposed_file_paths, max_file_path_count)
+      format_table(transposed_file_paths, max_basename_count)
     end
 
     private
 
-    def format_table(file_paths, max_file_path_count)
+    def format_table(file_paths, max_basename_count)
       file_paths.map do |row_files|
-        render_short_format_row(row_files, max_file_path_count)
+        render_short_format_row(row_files, max_basename_count)
       end.join("\n")
     end
 
-    def max_file_path_count
-      @collected_files.max_length_list[:basename]
-    end
-
-    def render_short_format_row(row_files, max_file_path_count)
+    def render_short_format_row(row_files, max_basename_count)
       row_files.map do |file_path|
         basename = file_path ? ::File.basename(file_path) : ''
-        basename.ljust(max_file_path_count + MARGIN_BETWEEN_FILE_NAME)
+        basename.ljust(max_basename_count + MARGIN_BETWEEN_FILE_NAME)
       end.join.rstrip
+    end
+
+    def transposed_file_paths
+      file_paths = @collected_files.paths
+      col_count = @width / (max_basename_count + MARGIN_BETWEEN_FILE_NAME)
+      row_count = col_count.zero? ? file_paths.count : (file_paths.count.to_f / col_count).ceil
+      safe_transpose(file_paths.each_slice(row_count).to_a)
     end
 
     def safe_transpose(nested_file_name)
       nested_file_name[0].zip(*nested_file_name[1..-1])
     end
 
-    def transposed_file_paths
-      file_paths = @collected_files.paths
-      col_count = @width / (max_file_path_count + MARGIN_BETWEEN_FILE_NAME)
-      row_count = col_count.zero? ? file_paths.count : (file_paths.count.to_f / col_count).ceil
-      safe_transpose(file_paths.each_slice(row_count).to_a)
+    def max_basename_count
+      @collected_files.max_length_list[:basename]
     end
   end
 end
